1ee5cc4c8588fdb20c3969d569f12b02e32cf8a3
ADJEVI Koffi Eyram Dénis <125583111+Deniscius@users.noreply.github.com>
2024-09-03 17:13:41 +0000

    new

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Responses\ApiResponse;
+
+class UserController extends Controller
+{
+    public function index()
+   {
+        //ApiResponse(true, [auth()->user()getAuthIdentifier->name], '...')
+
+        return ApiResponse::sendResponse(true, [new UserResource(User::all())], 'Opération effectuée.', 201);
+   } 
+
+   public function create(){
+
+   }
+
+   public function store(){
+    
+   }
+   public function show(){
+    
+   }
+   public function edit(){
+    
+   }
+   public function update(){
+    
+   }
+    
+}
